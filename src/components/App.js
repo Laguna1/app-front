e9a793071da65a32e9d9d32718e1f83f76367dc3
Laguna1 <lagunazoomarket@gmail.com>
2021-05-11 02:24:07 +0300

    Add dependencies & components folder

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="h1">Hi there!!!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Header = () => (
+
+  <header>HEADER</header>
+
+);
+
+export default Header;
