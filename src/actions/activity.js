333cf9b02f7f1f1276cb47ee735537b774aa0587
Laguna1 <lagunazoomarket@gmail.com>
2021-05-13 02:59:26 +0300

    Add activities&trackings actions

---
+++ src/actions/activity.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+
+export const DISPLAY_FETCHED_ACTIVITY = 'DISPLAY FETCHED ACTIVITY';
+export const CREATE_ACTIVITY = 'CREATE ACTIVITY';
+export const DELETE_ACTIVITY = 'DELETE ACTIVITY';
+export const CREATE_ACTIVITY_ERROR = 'CREATE ACTIVITY ERROR';
+export const UPDATE_ACTIVITY = 'UPDATE ACTIVITY';
+
+export const fetchUserActivity = (id) => (dispatch) => axios.get(`https://final-api-backend.herokuapp.com/users/${id}/activities`)
+  .then((response) => response.data)
+  .then((data) => {
+    dispatch({
+      type: DISPLAY_FETCHED_ACTIVITY,
+      payload: data,
+    });
+  })
+  .catch((error) => {
+    throw (error);
+  });
+
+export const createActivity = (data) => async (dispatch) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `https://final-api-backend.herokuapp.com/users/${data.user_id}/activities`,
+      data,
+      crossdomain: true,
+      withCredentials: true,
+    });
+    dispatch({
+      type: CREATE_ACTIVITY,
+      data: {
+        ...data,
+        id: response.data.id ? response.data.id : null,
+      },
+
+    });
+  } catch (error) {
+    dispatch({ type: CREATE_ACTIVITY_ERROR, payload: error });
+  }
+};
+
+export const deleteActivity = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: DELETE_ACTIVITY, payload: data });
+    const response = await axios({
+      method: 'DELETE',
+      url: `https://final-api-backend.herokuapp.com/users/${data.user_id}/activities/${data.id}`,
+      data,
+      crossdomain: true,
+      withCredentials: true,
+    });
+    return response;
+  } catch (error) {
+    return (error);
+  }
+};
+
+export const updateActivity = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_ACTIVITY, payload: data });
+    const response = await axios({
+      method: 'PATCH',
+      url: `https://final-api-backend.herokuapp.com/users/${data.user_id}/activities/${data.id}`,
+      data,
+      crossdomain: true,
+      withCredentials: true,
+    });
+    return response;
+  } catch (error) {
+    return (error);
+  }
+};
---
+++ src/actions/tracking.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+
+export const DISPLAY_FETCHED_ITEMS = 'DISPLAY FETCHED ITEMS';
+export const CREATE_ITEM = 'CREATE ITEM';
+export const DELETE_ITEM = 'DELETE ITEM';
+export const CREATE_ITEM_ERROR = 'CREATE ITEM ERROR';
+export const UPDATE_ITEM = 'UPDATE ITEM';
+
+export const fetchActivityItems = (userid, activityid) => (dispatch) => axios.get(`https://final-api-backend.herokuapp.com/users/${userid}/activities/${activityid}/trackings`)
+  .then((response) => response.data)
+  .then((data) => {
+    dispatch({
+      type: DISPLAY_FETCHED_ITEMS,
+      payload: data,
+    });
+  })
+  .catch((error) => {
+    throw (error);
+  });
+
+export const createItem = (data) => async (dispatch) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `https://final-api-backend.herokuapp.com/users/${data.user_id}/activities/${data.activity_id}/trackings `,
+      data,
+      crossdomain: true,
+      withCredentials: true,
+    });
+    dispatch({
+      type: CREATE_ITEM,
+      data: {
+        ...data,
+        id: response.data.id ? response.data.id : null,
+      },
+
+    });
+  } catch (error) {
+    dispatch({ type: CREATE_ITEM_ERROR, payload: error });
+  }
+};
+
+export const deleteItem = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: DELETE_ITEM, payload: data });
+    const response = await axios({
+      method: 'DELETE',
+      url: `https://final-api-backend.herokuapp.com/users/${data.user_id}/activities/${data.activity_id}/trackings/${data.id}`,
+      data,
+      crossdomain: true,
+      withCredentials: true,
+    });
+    return response;
+  } catch (error) {
+    return (error);
+  }
+};
+
+export const updateItem = (data) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_ITEM, payload: data });
+    const response = await axios({
+      method: 'PATCH',
+      url: `https://final-api-backend.herokuapp.com/users/${data.user_id}/activities/${data.activity_id}/trackings/${data.id}`,
+      data,
+      crossdomain: true,
+      withCredentials: true,
+    });
+    return response;
+  } catch (error) {
+    return (error);
+  }
+};
