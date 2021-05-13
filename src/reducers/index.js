038a79fbded331694bd817007454116503ffdc0b
Laguna1 <lagunazoomarket@gmail.com>
2021-05-13 03:39:51 +0300

    Add tracking reducer & state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import activityReducer from './activityReducer';
+import trackingReducer from './trackingReducer';
 
 export default combineReducers({
   user: userReducer,
   activity: activityReducer,
+  tracking: trackingReducer,
 });
