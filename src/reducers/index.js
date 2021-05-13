b6639af14144b5824511ba382ffeffe603ce2193
Laguna1 <lagunazoomarket@gmail.com>
2021-05-13 03:28:33 +0300

    Add activityReducer to Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
+import activityReducer from './activityReducer';
 
 export default combineReducers({
   user: userReducer,
+  activity: activityReducer,
 });
