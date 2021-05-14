b9922ec9ce6490d4621a70727ef197201c52557b
Laguna1 <lagunazoomarket@gmail.com>
2021-05-14 04:09:30 +0300

    Add activity & tracking form

--- src/components/ActivityForm/ActivityForm.js
+++ src/components/ActivityForm/ActivityForm.js
@@ -1,11 +1,176 @@
 import React from 'react';
 import './ActivityForm.css';
-// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { updateActivity } from '../../actions/activity';
 
-const ActivityForm = () => (
+class ActivityForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      place: '',
+      intensity: '',
+    };
+  }
 
-  <div className="a-form">ActivityForm</div>
+    componentDidMount = () => {
+      const { actionToPerform, buttonId, activity } = this.props;
+      if (actionToPerform === 'Save Changes') {
+        const activ = activity.filter((x) => x.id.toString() === buttonId);
+        this.setState({
+          name: activ[0].name,
+          place: activ[0].place,
+          intensity: activ[0].intensity,
+        });
+      }
+    }
 
-);
+    handleChangeName = (e) => {
+      this.setState({
+        name: e.target.value,
+      });
+    }
 
-export default ActivityForm;
+    handleChangePlace = (e) => {
+      this.setState({
+        place: e.target.value,
+      });
+    }
+
+    handleChangeIntensity = (e) => {
+      this.setState({
+        intensity: e.target.value,
+      });
+    }
+
+    handleSubmit = (name, place, intensity) => {
+      const { addActivity } = this.props;
+      addActivity(name, place, intensity);
+    }
+
+    handleUpdate = async (id) => {
+      const { name, place, intensity } = this.state;
+
+      const {
+        user, updateActivity, changeEditForm,
+      } = this.props;
+
+      const data = {
+        id,
+        user_id: user.user.id,
+        name,
+        place,
+        intensity,
+      };
+
+      await updateActivity(data);
+      changeEditForm();
+    }
+
+    render() {
+      const { name, place, intensity } = this.state;
+      const {
+        actionToPerform, activity, buttonId, changeEditForm, changeAddForm,
+      } = this.props;
+      const activ = activity.filter((x) => x.id.toString() === buttonId);
+      return (
+        <div className="a-form">
+          <h3>
+            {actionToPerform}
+            {' '}
+            activity
+          </h3>
+          <form
+            className="one-form"
+            onSubmit={
+             actionToPerform === 'Add'
+               // eslint-disable-next-line max-len
+               ? () => this.handleSubmit(name, place, intensity) : () => this.handleUpdate(activ[0].id)
+  }
+          >
+            <div className="one-parameter">
+              <label htmlFor="name">
+                Name:
+                <input
+                  required
+                  id="name"
+                  type="text"
+                  name="name"
+                  defaultValue={buttonId === '0' ? name : activ[0].name}
+                  onChange={this.handleChangeName}
+                />
+              </label>
+            </div>
+            <div className="one-parameter">
+              <label htmlFor="place">
+                Place:
+                <textarea
+                  id="place"
+                  name="place"
+                  defaultValue={buttonId === '0' ? place : activ[0].place}
+                  onChange={this.handleChangePlace}
+                />
+              </label>
+
+            </div>
+            <div className="one-parameter">
+              <label htmlFor="intensity">
+                intensity:
+                <textarea
+                  id="intensity"
+                  name="intensity"
+                  defaultValue={buttonId === '0' ? intensity : activ[0].place}
+                  onChange={this.handleChangeIntensity}
+                />
+              </label>
+
+            </div>
+            <div className="buttons-form">
+              {actionToPerform === 'Add' && <button type="submit">{actionToPerform}</button>}
+              {actionToPerform === 'Save Changes' && <button type="submit">Save</button>}
+              {actionToPerform === 'Add' && <button type="button" onClick={changeAddForm}>Cancel</button>}
+              {actionToPerform === 'Save Changes' && <button type="button" onClick={changeEditForm}>Cancel</button>}
+            </div>
+          </form>
+        </div>
+      );
+    }
+}
+
+ActivityForm.propTypes = {
+  addActivity: PropTypes.func,
+  actionToPerform: PropTypes.string,
+  changeAddForm: PropTypes.func,
+  activity: PropTypes.instanceOf(Array),
+  buttonId: PropTypes.string,
+  updateActivity: PropTypes.func,
+  changeEditForm: PropTypes.func,
+  user: PropTypes.shape({
+    user: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }),
+
+};
+
+ActivityForm.defaultProps = {
+  changeAddForm: () => {},
+  addActivity: () => {},
+  actionToPerform: '',
+  activity: [],
+  buttonId: '0',
+  updateActivity: () => {},
+  changeEditForm: () => {},
+  user: {},
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  activity: state.activity,
+});
+const mapDispatchToProps = (dispatch) => ({
+  updateActivity: (data) => dispatch(updateActivity(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActivityForm);
--- src/components/TrackingForm/TrackingForm.js
+++ src/components/TrackingForm/TrackingForm.js
@@ -1,11 +1,247 @@
 import React from 'react';
 import './TrackingForm.css';
-// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { updateItem } from '../../actions/tracking';
 
-const TrackingForm = () => (
+class TrackingForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '2020-13-05',
+      distance: 5000,
+      duration: 60,
+      pulse: 80,
+      calories: 150,
+      selectedOption: '🙂',
+    };
+  }
 
-  <div className="t-form">TrackingForm</div>
+  componentDidMount = () => {
+    const { actionToPerform, buttonId, trackings } = this.props;
+    if (actionToPerform === 'Save Changes') {
+      const track = trackings.filter((x) => x.id.toString() === buttonId);
+      this.setState({
+        date: track[0].date,
+        distance: track[0].distance,
+        duration: track[0].duration,
+        pulse: track[0].pulse,
+        calories: track[0].calories,
+        selectedOption: track[0].rate,
+      });
+    }
+  }
 
-);
+    handleChangeDate = (e) => {
+      this.setState({
+        date: e.target.value,
+      });
+    }
 
-export default TrackingForm;
+    handleChangeDistance = (e) => {
+      this.setState({
+        distance: e.target.value,
+      });
+    }
+
+    handleChangeDuration = (e) => {
+      this.setState({
+        duration: e.target.value,
+      });
+    }
+
+    handleChangePulse = (e) => {
+      this.setState({
+        pulse: e.target.value,
+      });
+    }
+
+    handleChangeCalories = (e) => {
+      this.setState({
+        calories: e.target.value,
+      });
+    }
+
+    handleOptionChange= (e) => {
+      this.setState({
+        selectedOption: e.target.value,
+      });
+    }
+
+    handleEdit = async (id, activityId) => {
+      const {
+        date, duration, selectedOption, distance, pulse, calories,
+      } = this.state;
+      const {
+        user, updateItem, changeEditForm,
+      } = this.props;
+      const data = {
+        id,
+        userId: user.user.id,
+        activityId,
+        date,
+        distance,
+        duration,
+        pulse,
+        calories,
+        rate: selectedOption,
+      };
+
+      await updateItem(data);
+      changeEditForm();
+    }
+
+    handleSubmit(date, duration, distance, pulse, calories, rate) {
+      const { addTracking, changeAddForm, user } = this.props;
+      const userId = user.user.id;
+      addTracking(date, duration, distance, pulse, calories, rate, userId);
+      changeAddForm();
+    }
+
+    render() {
+      const {
+        date, distance, duration, pulse, calories, selectedOption,
+      } = this.state;
+      const {
+        actionToPerform, trackings, buttonId, changeAddForm, changeEditForm,
+      } = this.props;
+      const track = trackings.filter((x) => x.id.toString() === buttonId);
+
+      return (
+        <div className="t-form">
+          <h3>
+            {actionToPerform}
+            {' '}
+            Tracking for Activity
+          </h3>
+          <form className="item">
+
+            <div className="form-div">
+              <div className="date-temp">
+                <div className="date-div">
+                  <label htmlFor="date">
+                    Date:
+                    <input
+                      id="date"
+                      type="date"
+                      name="date"
+                      defaultValue={buttonId === '0' ? date : track[0].date.slice(0, 10)}
+                      onChange={this.handleChangeDate}
+                    />
+                  </label>
+                </div>
+                <div className="duration-div">
+                  <label htmlFor="duration">
+                    distance:
+                    <input
+                      id="duration"
+                      type="number"
+                      name="duration"
+                      defaultValue={buttonId === '0' ? duration : track[0].duration}
+                      onChange={this.handleChangeDuration}
+                    />
+                  </label>
+                  <span>hours</span>
+                </div>
+              </div>
+              <div className="distance-div">
+                <label htmlFor="distance">
+                  distance:
+                  <input
+                    id="distance"
+                    type="number"
+                    name="distance"
+                    defaultValue={buttonId === '0' ? distance : track[0].distance}
+                    onChange={this.handleChangeDistance}
+                  />
+                </label>
+                <span>km</span>
+              </div>
+
+              <div className="pulse">
+                <p>Pulse:</p>
+                <div className="pulse-div">
+                  <input
+                    id="pulse"
+                    type="text"
+                    name="pulse"
+                    defaultValue={buttonId === '0' ? '' : track[0].pulse[0]}
+                    placeholder="Add pulse"
+                    onChange={(e) => this.handleChangePulse(e, 0, 0)}
+                  />
+                </div>
+              </div>
+              <div className="calories">
+                <p>calories:</p>
+                <div className="calories-div">
+                  <input
+                    id="calories"
+                    type="text"
+                    name="calories"
+                    placeholder="Used calories"
+                    defaultValue={buttonId === '0' ? '' : track[0].calories[0]}
+                    onChange={(e) => this.handleChangeCalories(e, 0)}
+                  />
+                </div>
+              </div>
+              <div className="rate-div">
+                <span>Rate: </span>
+                <input type="radio" id="option1" name="rate" value="🙂" checked={selectedOption === '🙂'} onChange={this.handleOptionChange} />
+                <span role="img" aria-label="rate_0">🙂</span>
+                <input type="radio" id="option2" name="rate" value="😐" checked={selectedOption === '😐'} onChange={this.handleOptionChange} />
+                <span role="img" aria-label="rate_1">😐</span>
+                <input type="radio" id="option3" name="rate" value="🙁" checked={selectedOption === '🙁'} onChange={this.handleOptionChange} />
+                <span role="img" aria-label="rate_2">🙁</span>
+                <input type="radio" id="option4" name="rate" value="😩" checked={selectedOption === '😩'} onChange={this.handleOptionChange} />
+                <span role="img" aria-label="rate_3">😩</span>
+              </div>
+
+              <div className="buttons-form item-buttons">
+                {actionToPerform === 'Add' && <button type="button" onClick={() => this.handleSubmit(selectedOption, distance, date, duration, pulse, calories)}>{actionToPerform}</button>}
+                {actionToPerform === 'Save Changes' && <button type="button" onClick={() => this.handleEdit(track[0].id, track[0].activityId)}>Save</button>}
+                {actionToPerform === 'Add' && <button type="button" onClick={changeAddForm}>Cancel</button>}
+                {actionToPerform === 'Save Changes' && <button type="button" onClick={changeEditForm}>Cancel</button>}
+              </div>
+            </div>
+          </form>
+        </div>
+      );
+    }
+}
+
+TrackingForm.propTypes = {
+  addTracking: PropTypes.func,
+  changeEditForm: PropTypes.func,
+  changeAddForm: PropTypes.func,
+  updateItem: PropTypes.func,
+  buttonId: PropTypes.string,
+  actionToPerform: PropTypes.string,
+  trackings: PropTypes.instanceOf(Array),
+  user: PropTypes.shape({
+    user: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }),
+};
+
+TrackingForm.defaultProps = {
+  changeAddForm: () => {},
+  addTracking: () => {},
+  updateItem: () => {},
+  changeEditForm: () => {},
+  actionToPerform: '',
+  trackings: [],
+  buttonId: '0',
+  user: {},
+
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  trackings: state.tracking,
+});
+const mapDispatchToProps = (dispatch) => ({
+  updateItem: (data) => dispatch(updateItem(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrackingForm);
