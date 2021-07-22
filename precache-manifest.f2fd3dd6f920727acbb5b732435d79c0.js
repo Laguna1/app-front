d734e8ab209fa936f6e45f563e22e39432fc019d
Laguna1 <lagunazoomarket@gmail.com>
2021-07-22 03:32:47 +0300

    Updates

---
+++ precache-manifest.f2fd3dd6f920727acbb5b732435d79c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7984b163649f722c2807",
+    "url": "/app-front/static/css/main.f72616fd.chunk.css"
+  },
+  {
+    "revision": "7984b163649f722c2807",
+    "url": "/app-front/static/js/main.7984b163.chunk.js"
+  },
+  {
+    "revision": "3e9287d26c6f1c32ff32",
+    "url": "/app-front/static/css/1.2da947a6.chunk.css"
+  },
+  {
+    "revision": "3e9287d26c6f1c32ff32",
+    "url": "/app-front/static/js/1.3e9287d2.chunk.js"
+  },
+  {
+    "revision": "0fa793fc3017265c944e",
+    "url": "/app-front/static/js/runtime~main.0fa793fc.js"
+  },
+  {
+    "revision": "026426c26d065cfa7a013a6dd747809d",
+    "url": "/app-front/index.html"
+  }
+];
